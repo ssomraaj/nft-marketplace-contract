9da3aa286a07085bc18da43e499b1e938c806b6d
Sujith Somraaj <sujithsomraaj@Sujiths-iMac.local>
2021-05-24 00:17:08 +0530

    DAO SCs & BEP20 SCs

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -2,6 +2,82 @@
 
 pragma solidity ^0.8.4;
 
-contract DAO {
+import "./interfaces/IDAO.sol";
+import "./utils/Context.sol";
+import "./interfaces/IBEP20.sol";
+
+contract DAO is Context, IDAO{
+    struct Proposal{
+      bytes hash;
+      address merchant;
+      uint256 votes;
+      bool approved;
+    }
     
+    address private _token;
+
+    uint256 private _merchantsCount;
+    uint256 private _proposalsCount;
+
+    mapping(address => bool) private _merchant;
+    mapping(uint256 => Proposal) private _proposal;
+
+    constructor(address _tokenContract) {
+        _token = _tokenContract;
+    }
+
+    function createMerchant(string memory hash) public virtual override returns (bool) {
+        _proposalsCount += 1;
+
+        _proposal[_proposalsCount] = Proposal(
+            bytes(hash),
+            _msgSender(),
+            0,
+            false
+        );
+        return true;
+    }    
+
+    function vote(uint256 proposalId) public virtual override returns (bool) {
+        uint256 balance = IBEP20(_token).balanceOf(_msgSender());
+        uint256 totalSupply = IBEP20(_token).totalSupply();
+
+        require(balance > 0, "Error: Voter should have ZNFT Shares");
+        require(proposalId > 0 && proposalId <= _proposalsCount, "Error: Invalid Proposal ID");
+
+        Proposal storage p = _proposal[proposalId];
+        require(!p.approved, "Error: Proposal already approved");
+
+        p.votes += balance;
+        if(p.votes > totalSupply / 2) {
+            p.approved = true;
+            _merchantsCount += 1;
+            _merchant[p.merchant] = true;
+        }
+        return true;
+    }
+
+    function isMerchant(address _merchantAddress) public virtual view returns (bool) {
+        return _merchant[_merchantAddress];
+    }
+
+    function totalProposals() public virtual view returns (uint256) {
+        return _proposalsCount;
+    }
+
+    function totalMerchants() public virtual view returns (uint256) {
+        return _merchantsCount;
+    }
+
+    function proposal(uint256 proposalId) public virtual view returns (string memory hash, address merchant, uint256 totalVotes, bool approved) {
+        require(proposalId > 0 && proposalId <= _proposalsCount, "Error: Invalid Proposal ID");
+
+        Proposal storage p = _proposal[proposalId];
+        return(
+          string(p.hash),
+          p.merchant,
+          p.votes,
+          p.approved
+        );
+    }
 }
--- contracts/interfaces/IDAO.sol
+++ contracts/interfaces/IDAO.sol
@@ -3,5 +3,18 @@
 pragma solidity ^0.8.4;
 
 interface IDAO {
-    
+    /**
+     * @dev receives the listing for adding new merchants to the marketplace.
+     *
+     *`hash` is the ipfs hash of the company-info JSON. To reduce gas usuage we're following this approach.
+     */
+    function createMerchant(string memory hash) external returns (bool);
+
+    /**
+     * @dev vote for the approval of merchants.
+     *
+     * `proposalId` will be the listing Id of the proposal.
+     */
+    function vote(uint256 proposalId) external returns (bool);
+
 }
